ccd0925242c4f5780e719ea913efc13026b32dd8
Alain <alaindelonm@gmail.com>
2022-07-01 11:31:52 -0300

    change title

--- src/components/Navbar/styles.js
+++ src/components/Navbar/styles.js
@@ -2,9 +2,14 @@ import styled from "styled-components";
 
 export const Container = styled.div`
     display: flex;
+    margin-top: -40px;
+    margin-left: -50px;
+    margin-right: -50px;
     margin-bottom: 2rem;
     justify-content: center;
     flex-wrap: wrap;
+    background-color: rgb(20,76,152);
+    color: aliceblue;
 
     h1 {
         font-size: 4rem;
@@ -13,5 +18,6 @@ export const Container = styled.div`
     h2 {
         font-size: 2rem;
         align-self: center;
+        padding: 2rem;
     }
 `;
